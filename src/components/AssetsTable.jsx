0641a7547096e8f8e458cd1ca7f629d9037f37e1
AlexeyYakovec <yakovec8952@gmail.com>
2024-06-06 12:08:39 +0500

    Create a chart using the react-chartjs-2 library

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Table } from "antd";
+
+import CryptoContext from "../context/crypto-context";
+
+const columns = [
+   {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: {
+         target: "full-header",
+      },
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ["descend"],
+   },
+   {
+      title: "Price $",
+      dataIndex: "price",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.price - b.price,
+   },
+   {
+      title: "Amount",
+      dataIndex: "amount",
+      sorter: (a, b) => a.amount - b.amount,
+   },
+];
 
 export default function AssetsTable() {
-   return <div>AssetsTable</div>;
+   const { assets } = useContext(CryptoContext);
+
+   const data = assets.map((asset) => ({
+      key: asset.id,
+      name: asset.name,
+      price: asset.price,
+      amount: asset.amount,
+   }));
+
+   return <Table pagination={false} columns={columns} dataSource={data} />;
 }
